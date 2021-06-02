47655cb93bcb115b9980a694c1a83570d4e5e4df
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2021-06-02 18:38:37 -0500

    Initial Commit: 121. Finding the Path

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float speed  = 1f;
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+            //Traverse the path using interpolation between the current position of the enemy and the next waypoint.
+            while(travelPercent < 1f)
+            {
+                //Updates the travelPercent (0-1) based in the time.deltaTime
+                //Travlel Percent = 0 the position will be the same as startPosition.
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
