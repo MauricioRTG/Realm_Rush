d81192994ce0c3d07f55edfb8f3264db08ddcaa0
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2021-07-25 21:09:08 -0500

    143. Broadcast Message

--- Assets/PathFinding/Pathfinder.cs
+++ Assets/PathFinding/Pathfinder.cs
@@ -148,4 +148,9 @@ public class Pathfinder : MonoBehaviour
         return false;
     }
 
+    public void NotifyReceivers()
+    {
+        BroadcastMessage("RecalculatePath", SendMessageOptions.DontRequireReceiver);
+    }
+
 }
